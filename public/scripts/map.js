f5a5390e66084ca4dbc8a1625da73027938362d7
Mike Dragert <mikedragert@gmail.com>
2024-04-19 19:32:45 -0400

    Rewrite program to use classes for marker/line/point. Track each class created and still on the map, including coordinates and tooltip text

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,20 +1,15 @@
+import {modeType, Marker, Line, Rectangle } from './mapObjects.js';
 
-const modeType = {
-  POINT: 1,
-  LINE: 2,
-  RECTANGLE: 3
-}
 
-const LINEWEIGHT = 4;
-const LINEOPACITY = 0.75;
-const POINTCLICKBOUNDS = 0.005;  //this maybe should scale with the zoom
-
-let points = [];
-let savedTooltipText = '';
-let objectJustClicked = false;
+let map = undefined;
+let mapObjects = [];
 
 let mode = modeType.POINT;
-let currentObject = undefined;
+let ignoreNextClick = false;
+
+let setIgnoreNextClick = function() {
+  ignoreNextClick = true;
+}
 
 let setMode = function(newMode) {
   if (Object.values(modeType).includes(newMode)) {
@@ -22,93 +17,45 @@ let setMode = function(newMode) {
   }
 }
 
-let pointsClose = function(point1, point2) {
-  return (point1.lat - POINTCLICKBOUNDS) <= point2.lat && 
-          point2.lat <= (point1.lat + POINTCLICKBOUNDS) &&
-         (point1.lng - POINTCLICKBOUNDS) <= point2.lng && 
-          point2.lng <= (point1.lng + POINTCLICKBOUNDS); 
+const getCurrentEditingTooltipObject = function() {
+  return mapObjects.find((mapObject) => mapObject.editingTooltip)
 }
 
-let currentTooltipObject = undefined;
-let currentTooltipText = '';
-
-const setCurrentTooltipText = function(newText, editing) {
-  currentTooltipText = newText;
-  if (editing) {
-    currentTooltipObject.setContent(`<strong>${currentTooltipText}</strong>`);
-  } else {
-    currentTooltipObject.setContent(currentTooltipText);
-  }
+const getCurrentEditingSizeObject = function() {
+  return mapObjects.find((mapObject) => mapObject.editingSize)
 }
 
-const createTooltip = function (map, element, content = '') {
-  element.bindTooltip(content, {permanent: true, direction: "auto", interactive: true, bubblingMouseEvents: false})
-    .openTooltip()
-    .on('click', (e) => {
-      if (tooltipMatch(element, e)) {
-        toggleTooltipEdit(map, element.getTooltip());   
-      }
-    });
 
-  if (content.length === 0) {
-    toggleTooltipEdit(map, element.getTooltip());
-  }
+const clearAllEditingObjects = function(exception) {
+  mapObjects.forEach( mapObject => {
+    if ((mapObject !== exception) && (mapObject.editingTooltip)) {
+      mapObject.toggleTooltipEdit(map);
+    }
+  })
 }
 
-const tooltipMatch = function(element, event) {
-  return element.getTooltip() === event.sourceTarget;
+const removeMapObject = function(removeObject) {
+  mapObjects = mapObjects.filter(mapObject => mapObject !== removeObject);
 }
 
-let toggleTooltipEdit = function(map, tooltipObject) {
-  if (!tooltipObject) {
-    $('#map').focus();
-    map.keyboard.enable();
-    currentTooltipObject = undefined;
-    return;
-  }
-
-  if (currentTooltipObject === tooltipObject) {
-    setCurrentTooltipText(currentTooltipText, false);
-    $('#map').focus();
-    map.keyboard.enable();
-    currentTooltipObject = undefined;
-    return;
-  }
-
-  if (currentTooltipObject !== undefined) {
-    setCurrentTooltipText(currentTooltipText, false);
-    currentTooltipObject = undefined;
-  }
-
-  currentTooltipText = tooltipObject.getContent();
-  currentTooltipObject = tooltipObject;
-  setCurrentTooltipText(currentTooltipText, true);
-  map.keyboard.disable(); //todo: something weird happens here.  enabling keyboard later doesn't work until we click outside the map and then back in
-}
-
-let handleKey = function(keyEvent) {
-  if (currentTooltipObject !== undefined) {
+const handleKey = function(keyEvent) {
+  let currentObjectEditing = getCurrentEditingTooltipObject();
+  if (currentObjectEditing) {
     if ((keyEvent.keyCode === 8) || (keyEvent.keyCode === 127)) {
-      setCurrentTooltipText(currentTooltipText.slice(0,-1), true);      
+      currentObjectEditing.removeLastCharFromTooltipContent();       
     } else if (keyEvent.keyCode === 13) {
-      setCurrentTooltipText(currentTooltipText + "<br>", true);
+      currentObjectEditing.addCharToTooltipContent("<br>"); 
     } else if (keyEvent.keyCode === 32) {
       keyEvent.stopPropagation();
       keyEvent.preventDefault();
-      setCurrentTooltipText(currentTooltipText + keyEvent.key, true);
+      currentObjectEditing.addCharToTooltipContent(keyEvent.key); 
     } else if (keyEvent.keyCode >= 48 ) {
       keyEvent.stopPropagation();
       keyEvent.preventDefault();
-      setCurrentTooltipText(currentTooltipText + keyEvent.key, true);
+      currentObjectEditing.addCharToTooltipContent(keyEvent.key);
     }
   }
-
 }
-
-//todo:  we have an issue where things like removing a marker, or editing a tooltip text
-//       put leaflet in a state where key commands don't work
-//     However, clicking outside and then back inside the map seem to fix it.  Weird...
-//     Also attempting keyboard entry puts a border around the map.  
 
 $(document).ready(function() {
   
@@ -119,148 +66,46 @@ $(document).ready(function() {
   $("#lineButton").on('click', () => setMode(modeType.LINE));
   $("#rectangleButton").on('click', () => setMode(modeType.RECTANGLE));
 
-
   //todo:  what is the default location - can we get the location of the user?
-  let map = L.map('map').setView([51.505, -0.09], 13);
+  map = L.map('map').setView([51.505, -0.09], 13);
   
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-
-  
-  let createMarker = function(currentPoints) {
-    if (currentPoints.length < 1) {
-      return;
-    }
-    let marker = L.marker(points[0]);
-    marker.on('click', (e) => {
-      if (marker === e.sourceTarget) {
-        map.removeLayer(marker);
-        $('#map').focus();
-      }
-    });
-
-   
-    marker.addTo(map);
-    createTooltip(map, marker);
-    
-    points = [];
-  }
-  
-  let drawMapObject = function(currentPoints, currentMode, tooltipText, drawing = false) {
-    if (currentPoints.length < 2) {
-      return;
-    }
-    let color = drawing ? "CornflowerBlue" : "MediumSlateBlue"
-    let newMapObject = undefined;
-    
-    if (currentMode === modeType.LINE) {
-      newMapObject = new L.Polyline(currentPoints, {
-          color: color,
-          weight: 3,
-          opacity: 0.75,
-          smoothFactor: 1
-      });
-    }
-
-    if (currentMode === modeType.RECTANGLE) {
-      newMapObject = new L.Rectangle(currentPoints, {
-          color: color,
-          weight: LINEWEIGHT,
-          opacity: LINEOPACITY,
-          smoothFactor: 1
-      });
-    }
-
-    if (!newMapObject) {
-      return;
-    }
-
-    newMapObject.on('click', (e) => {
-      //todo: can me make markers, lines, rectangles movable by click drag?
-      if (newMapObject === e.sourceTarget) {
-        let notStillDrawing = points.length === 0;
-
-        if (notStillDrawing) {
-          //did we click near a corner?
-          let objectLatLngs = [];
-          let totalPoints = 0;
-          if (newMapObject instanceof L.Polyline) {
-            mode = modeType.LINE;
-            objectLatLngs = newMapObject.getLatLngs();
-            totalPoints = 2;
-          }
-          if (newMapObject instanceof L.Rectangle) {
-            mode = modeType.RECTANGLE;
-            objectLatLngs = newMapObject.getLatLngs()[0];
-            totalPoints = 4;
-          }
-          if (objectLatLngs.length > 1) {
-            for(let index = 0; index < objectLatLngs.length; index++) {
-              if (pointsClose(e.latlng, objectLatLngs[index])) {
-                let keepPoint = totalPoints === 4? (index + 2) % totalPoints : (index + 1) % totalPoints;
-                points = [objectLatLngs[keepPoint]];
-                break;
-              }
-            }
-          }
-          savedTooltipText = newMapObject.getTooltip().getContent();
-          map.removeLayer(newMapObject);
-          
-          drawMapObject([...points, e.latlng], mode, tooltipText, true);
-
-          objectJustClicked = true;
-        }
-      }
-    });
-
-    if (currentObject) {
-      map.removeLayer(currentObject);
-    }
-    if (drawing) {
-      currentObject = newMapObject;
-    }
-
-    newMapObject.addTo(map);
-    if (savedTooltipText.length > 0) {
-      createTooltip(map, newMapObject,savedTooltipText);
-    } else if (!drawing) {
-      createTooltip(map, newMapObject);
-    }
-
-    if (!drawing) {
-      points = [];
-      savedTooltipText = '';
-    }
-  }
-
-
   //handle on click event for map
   map.on('click', (e) => {
-    if ((!objectJustClicked)) {
-      points.push(e.latlng)
-    } 
-    objectJustClicked = false;
-    
+    if (ignoreNextClick) {
+      ignoreNextClick = false;
+      return;
+    }
     switch(mode) {
       case modeType.POINT:
-        createMarker(points);
+        mapObjects.push(new Marker([e.latlng], { clearAllEditingObjects , removeMapObject, setIgnoreNextClick} ));
+        mapObjects[mapObjects.length-1].createOnMap(L, map);
+        console.log(mapObjects);
         break;
       case modeType.LINE:
-        case modeType.RECTANGLE:
-        drawMapObject(points, mode, undefined, false);
+        mapObjects.push(new Line([e.latlng], { clearAllEditingObjects , removeMapObject, setIgnoreNextClick} ));
+        mapObjects[mapObjects.length-1].createOnMap(L, map, e.latlng);
+        break;
+      case modeType.RECTANGLE:
+        mapObjects.push(new Rectangle([e.latlng], { clearAllEditingObjects , removeMapObject, setIgnoreNextClick} ));
+        mapObjects[mapObjects.length-1].createOnMap(L, map, e.latlng);
         break;
     }
   })
   
   map.on('mousemove', (e) => {
-    if (points.length > 0 )  {
+    //todo: we need to properly figure out what we are editing!
+    let editObject = getCurrentEditingSizeObject()
+    
+    if (editObject) {
       switch(mode) {
         case modeType.LINE:
         case modeType.RECTANGLE:
-          drawMapObject([...points, e.latlng], mode, undefined, true);
+          editObject.createOnMap(L, map, e.latlng);
           break;
       }
     } 
---
+++ public/scripts/mapObjects.js
@@ -0,0 +1,295 @@
+export const modeType = {
+  POINT: 1,
+  LINE: 2,
+  RECTANGLE: 3
+}
+
+const LINEWEIGHT = 4;
+const LINEOPACITY = 0.75;
+const POINTCLICKBOUNDS = 0.005;  //this maybe should scale with the zoom
+
+export class MapObject {
+  _type = undefined;
+  _points = [];
+  _tooltipContent = '';
+  _editingTooltip = false;
+  _mapElement = undefined;
+  _callbacks = undefined;
+  _editingSize = false;
+
+  constructor () {
+    this._editingTooltip = false;
+  }
+
+  get editingTooltip() {
+    return this._editingTooltip;
+  }
+
+  get type() {
+    return this._type;
+  }
+  
+  get numberPoints() {
+    return this._points.length
+  }
+  
+  get editingSize() {
+    return this._editingSize;
+  }
+
+  setPoints = function(points) {
+    this._points = points;
+  }
+
+  addPoint = function(point) {
+    this._points.push(point);
+  }
+
+  clearPoints = function() {
+    this._points = [];
+  }
+
+  removeLastPoint = function() {
+    this._points.pop();
+  }
+
+  setTooltipContent = function(tooltipContent) {
+    this._tooltipContent = tooltipContent;
+    this._displayCurrentTooltipText();
+  }
+  
+  clearTooltipContent = function() {
+    this._tooltipContent = '';
+    this._displayCurrentTooltipText();
+  }
+  
+  addCharToTooltipContent = function(char) {
+    this._tooltipContent += char;
+    this._displayCurrentTooltipText();
+  }
+  
+  removeLastCharFromTooltipContent = function () {
+    this._currentTooltipText.slice(0,-1);  
+    this._displayCurrentTooltipText();
+  }
+
+  _displayCurrentTooltipText = function() {
+    if (this._editingTooltip) {
+      this._mapElement.getTooltip().setContent(`<strong>${this._tooltipContent }</strong>`);
+    } else {
+      this._mapElement.getTooltip().setContent(this._tooltipContent );
+    }
+  }
+  
+  _createTooltip = function (map) {
+    if (!this._mapElement) {
+      return;
+    }
+
+    this._mapElement.bindTooltip(this._tooltipContent, {permanent: true, direction: "auto", interactive: true, bubblingMouseEvents: false})
+      .openTooltip()
+      .on('click', (event) => {
+        if (this._tooltipMatch(event)) {
+          this.toggleTooltipEdit(map);   
+        }
+      });
+  
+    if (this._tooltipContent.length === 0) {
+      this.toggleTooltipEdit(map);
+    }
+  }
+  
+  _tooltipMatch = function(event) {
+    if (!this._mapElement) {
+      return false;
+    }
+    return this._mapElement.getTooltip() === event.sourceTarget;
+  }
+  
+  toggleTooltipEdit = function(map) {
+    if (!this._mapElement) {
+      return;
+    }
+    this._editingTooltip = !this._editingTooltip;
+    
+    if (!this._mapElement.getTooltip()) {
+      $('#map').focus();
+      map.keyboard.enable();
+      return;
+    }
+
+    this._displayCurrentTooltipText();
+    $('#map').focus();
+  
+    if (this._editingTooltip) {
+      this._callbacks.clearAllEditingObjects(this);
+      map.keyboard.disable(); //todo: something weird happens here.  enabling keyboard later doesn't work until we click outside the map and then back in
+      return;
+    } 
+    map.keyboard.enable(); 
+  }
+  
+  _pointsClose = function(point1, point2) {
+    return (point1.lat - POINTCLICKBOUNDS) <= point2.lat && 
+            point2.lat <= (point1.lat + POINTCLICKBOUNDS) &&
+           (point1.lng - POINTCLICKBOUNDS) <= point2.lng && 
+            point2.lng <= (point1.lng + POINTCLICKBOUNDS); 
+  }
+
+  _createMarker = function(L, map) {
+    if (this._points.length < 1) {
+      return;
+    }
+
+    if (this._type = modeType.POINT) {
+      //todo: should we check _mapElement is undefined?
+      if (this._mapElement === undefined) {
+        this._mapElement = L.marker(this._points[0]);
+        this._mapElement.on('click', (e) => {
+          if (this._mapElement === e.sourceTarget) {
+            map.removeLayer(this._mapElement);
+            this._mapElement = undefined;
+            $('#map').focus();
+            this._callbacks.removeMapObject(this);
+          }
+        });
+
+        this._mapElement.addTo(map);
+        this._createTooltip(map);
+      }
+    }
+  }
+
+  _drawMapObject = function(L, map, targetPoint = undefined) {//;(currentPoints, currentMode, tooltipText, drawing = false) {
+    let targetPoints = [...this._points];
+    if ((targetPoints.length === 1) && (targetPoint instanceof L.LatLng)) {
+      targetPoints.push(targetPoint);
+    }
+    
+    if (targetPoints.length < 2) {
+      return;
+    }
+    let drawing = this._points.length < 2;
+    let color = drawing ? "CornflowerBlue" : "MediumSlateBlue"
+    let lastMapElement = this._mapElement;    
+    if (this._type === modeType.LINE) {
+      this._mapElement = new L.Polyline(targetPoints, {
+          color: color,
+          weight: LINEWEIGHT,
+          opacity: LINEOPACITY,
+          smoothFactor: 1
+      });
+    }
+
+    if (this._type === modeType.RECTANGLE) {
+      this._mapElement = new L.Rectangle(targetPoints, {
+          color: color,
+          weight: LINEWEIGHT,
+          opacity: LINEOPACITY,
+          smoothFactor: 1
+      });
+    }
+
+    if (!this._mapElement) {
+      return;
+    }
+
+    this._mapElement.on('click', (event) => {
+      //todo: can me make markers, lines, rectangles movable by click drag?
+      if (this._mapElement === event.sourceTarget) {
+        let stillDrawing = this._points.length !== 2;
+
+        if (stillDrawing) {
+          this._points.push(event.latlng);
+          this._editingSize = false;
+          map.removeLayer(this._mapElement);          
+          this._drawMapObject(L, map);
+        } else {
+          //did we click near a corner?
+          let objectLatLngs = [];
+          let totalPoints = 0;
+          if (this._type === modeType.LINE) {
+            objectLatLngs = this._mapElement.getLatLngs();
+            totalPoints = 2;
+          }
+          if (this._type === modeType.RECTANGLE) {
+            objectLatLngs = this._mapElement.getLatLngs()[0];
+            totalPoints = 4;
+          }
+          
+          let foundPoint = false;
+          if (objectLatLngs.length > 1) {
+            for(let index = 0; index < objectLatLngs.length; index++) {
+              if (this._pointsClose(event.latlng, objectLatLngs[index])) {
+                let keepPoint = totalPoints === 4? (index + 2) % totalPoints : (index + 1) % totalPoints;
+                this._points = [objectLatLngs[keepPoint]];
+                foundPoint = true;
+                break;
+              }
+            }
+          }
+          map.removeLayer(this._mapElement);  
+          if (foundPoint) {
+            this._drawMapObject(L, map, event.latlng);
+            this._editingSize = true;
+          } else {
+            this._callbacks.removeMapObject(this);
+          }
+        }
+      }
+      this._callbacks.setIgnoreNextClick();
+    });
+
+    if (lastMapElement) {
+      map.removeLayer(lastMapElement);
+    }
+    
+    this._mapElement.addTo(map);
+    this._createTooltip(map);
+  }
+
+  createOnMap = function(L, map, targetPoint = undefined) {
+    switch(this._type) {
+      case modeType.POINT:
+        this._createMarker(L, map);
+        return;
+      case modeType.LINE:
+      case modeType.RECTANGLE:
+        this._drawMapObject(L, map, targetPoint);
+        return;
+    }
+  }
+}
+
+export class Marker extends MapObject {
+  constructor (points, callbacks, tooltipContent = '') {
+    super();
+    this._type = modeType.POINT;
+    this._points = points;
+    this._tooltipContent = tooltipContent;
+    this._editingSize - false;
+    this._callbacks = callbacks;
+  }
+}
+
+export class Line extends MapObject {
+  constructor (points, callbacks, tooltipContent = '') {
+    super();
+    this._type = modeType.LINE;
+    this._points = points;
+    this._tooltipContent = tooltipContent;
+    this._editingSize = this._points.length < 2;
+    this._callbacks = callbacks;
+  }
+}
+
+export class Rectangle extends MapObject {
+  constructor (points, callbacks, tooltipContent = '') {
+    super();
+    this._type = modeType.RECTANGLE;
+    this._points = points;
+    this._tooltipContent = tooltipContent;
+    this._editingSize = this._points.length < 2;
+    this._callbacks = callbacks;
+  }
+}
